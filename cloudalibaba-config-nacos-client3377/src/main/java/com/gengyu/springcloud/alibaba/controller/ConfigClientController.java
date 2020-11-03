71452569e57d2019cadd3cb41c60655c91df9f36
Siegfried <bachbrahms@163.com>
2020-11-04 00:03:50 +0800

    addition

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/gengyu/springcloud/alibaba/controller/ConfigClientController.java
@@ -0,0 +1,19 @@
+package com.gengyu.springcloud.alibaba.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope   //支持Nacos的动态刷新功能（相当于原来的3355 Client，且是在没有BUS的情况下，需要手动发POST请求去刷新）
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
